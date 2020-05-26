4554e29e43723fcdfc18854e597501d07d71174d
Popov85 <andrey.popov85@gmail.com>
2020-05-26 17:03:51 +0300

    Service added

--- src/main/java/com/company/flywaytest/repository/EmployeeRepository.java
+++ src/main/java/com/company/flywaytest/repository/EmployeeRepository.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @EntityGraph(value = "Employee.min", type = EntityGraph.EntityGraphType.LOAD)
-    Set<Employee> findEmployeesByDepartment_DepId(Long depId);
+    Set<Employee> findEmployeesWithMinByDepartment_DepId(Long depId);
 
     @EntityGraph(value = "Employee.everything", type = EntityGraph.EntityGraphType.LOAD)
     Set<Employee> findEmployeesWithEverythingByDepartment_DepId(Long depId);
---
+++ src/main/java/com/company/flywaytest/service/EmployeeService.java
@@ -0,0 +1,27 @@
+package com.company.flywaytest.service;
+
+import com.company.flywaytest.entity.Employee;
+import com.company.flywaytest.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Transactional(readOnly = true)
+    public Set<Employee> findAllEmployeesWithMinByDepId(@NonNull final Long depId) {
+        return employeeRepository.findEmployeesWithMinByDepartment_DepId(depId);
+    }
+
+    @Transactional(readOnly = true)
+    public Set<Employee> findAllEmployeesWithEverythingByDepId(@NonNull final Long depId) {
+        return employeeRepository.findEmployeesWithEverythingByDepartment_DepId(depId);
+    }
+}
