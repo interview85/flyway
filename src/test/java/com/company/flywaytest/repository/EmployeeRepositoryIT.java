bfb661e5c543d03041171a0d8ae3d8321de7b9d6
Popov85 <andrey.popov85@gmail.com>
2020-05-26 17:50:19 +0300

    Renamed in test

--- src/test/java/com/company/flywaytest/repository/EmployeeRepositoryIT.java
+++ src/test/java/com/company/flywaytest/repository/EmployeeRepositoryIT.java
@@ -1,7 +1,6 @@
 package com.company.flywaytest.repository;
 
 import com.company.flywaytest.entity.Employee;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnitUtil;
@@ -17,10 +15,9 @@ import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@DataJpaTest
-//@ImportAutoConfiguration(FlywayAutoConfiguration.class)
+@DataJpaTest
+@ImportAutoConfiguration(FlywayAutoConfiguration.class)
 class EmployeeRepositoryIT {
 
     @Autowired
@@ -38,24 +35,9 @@ class EmployeeRepositoryIT {
 
     @Test
     @Disabled
-    //@Sql(scripts = {"/scripts/init.sql", "/scripts/course_test_data_many.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    //@Sql(scripts = "/scripts/clear.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void getAllEmployeesWithEverythingByDepId() {
-        int i = 2;
-        assertEquals(i, 1);
-        //Set<Employee> result = employeeRepository.getAllEmployeesWithEverything();
-        //assertThat("Not the right size!", result, hasSize(3));
-        // Fetched Access, Staff, Department
-        /*assertTrue("Access of Course is not loaded", persistenceUnitUtil.isLoaded(course.get(), "access"));
-        assertTrue("Staff of Course is not loaded", persistenceUnitUtil.isLoaded(course.get(), "staff"));
-        assertTrue("User of Staff is not loaded", persistenceUnitUtil.isLoaded(course.get().getStaff(), "user"));
-        assertTrue("Department of Staff is not loaded", persistenceUnitUtil.isLoaded(course.get().getStaff(), "department"));
-        assertThat("Course object is not as expected", course.get(), allOf(
-                hasProperty("courseId", equalTo(1L)),
-                hasProperty("name", equalTo("Test LTI course #1")),
-                hasProperty("created", is(notNullValue())),
-                hasProperty("deleted", equalTo(false)))
-        );*/
+        Set<Employee> result = employeeRepository.findEmployeesWithMinByDepartment_DepId(1L);
+        assertThat("Not the right size!", result, hasSize(3));
     }
 
 }
